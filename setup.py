340fc90696d9204fd22667ada4c187d77b451337
Lubos Matl <matllubos@gmail.com>
2013-11-14 10:28:25 +0100

    Allow pip install

--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
-raise NotImplementedError('Early development stage, instead of pip or setuptools please install by cloning the repository and placing it on the Python path.')
-
-# from setuptools import setup, find_packages
-#  
-# setup(
-#     name='django-html5-monkepatch',
-#     version='0.1',
-#     description='Django Automatic HTML5 through sane monkeypatching',
-#     author='Daniel Sokolowski',
-#     author_email='daniel.sokolowski@danols.com',
-#     packages=find_packages(),
-#     classifiers=[
-#         'Development Status :: 3 - Alpha',
-#         'Environment :: Web Environment',
-#         'Intended Audience :: Developers',
-#         'License :: OSI Approved :: Simplified BSD License',
-#         'Operating System :: OS Independent',
-#         'Programming Language :: Python',
-#         'Framework :: Django',
-#     ],
-#     include_package_data=True,
-#     zip_safe=False,
-# )
+from setuptools import setup, find_packages
+  
+setup(
+    name='django-html5-monkepatch',
+    version='0.1',
+    description='Django Automatic HTML5 through sane monkeypatching',
+    author='Daniel Sokolowski',
+    author_email='daniel.sokolowski@danols.com',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Simplified BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
